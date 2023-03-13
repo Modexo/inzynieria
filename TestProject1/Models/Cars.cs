2e12502f032c4ef70028bbc322e0464e16be6be1
Karol Ciupak <w64856@student.wsiz.edu.pl>
2023-03-13 15:59:38 +0100

    Lab2

---
+++ TestProject1/Models/Cars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject1.Models
+{
+    class Cars
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Vin { get; set; }
+        public string Fuel { get; set; }
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -14,12 +14,35 @@ namespace TestProject1
             Faker<UserAddress> addressBogus = new Faker<UserAddress>();
             addressBogus.RuleFor(x => x.Description, y => y.Address.FullAddress());
             addressBogus.RuleFor(x => x.Number, y => y.Address.CountryCode());
+
             var personBogus = new Faker<Models.Person>();
             personBogus.RuleFor(x => x.Name, y => y.Person.FirstName);
             personBogus.RuleFor(x => x.Surname, y => y.Person.LastName);
             personBogus.RuleFor(x => x.Birthday, y => y.Person.DateOfBirth);
             personBogus.RuleFor(x => x.Address, y => addressBogus.Generate());
+
             var hundredPpl = personBogus.Generate(100);
+        }
+
+
+    }
+
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test2()
+        {
+            Faker<CarOwner> carsBogus = new Faker<CarOwner>();
+            carsBogus.RuleFor(x => x.Name, y => y.Person.FirstName);
+            carsBogus.RuleFor(x => x.Surname, y => y.Person.LastName);
+            carsBogus.RuleFor(x => x.PhoneNumber, y => y.Phone.PhoneNumber());
+            carsBogus.RuleFor(x => x.Manufacturer, y => y.Vehicle.Manufacturer());
+            carsBogus.RuleFor(x => x.Model, y => y.Vehicle.Model());
+            carsBogus.RuleFor(x => x.Vin, y => y.Vehicle.Vin());
+            carsBogus.RuleFor(x => x.Fuel, y => y.Vehicle.Fuel());
+
+            var sevenCars = carsBogus.Generate(7);
+            
         }
     }
 }
